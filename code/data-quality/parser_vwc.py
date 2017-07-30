2ba9d325b9a555f28aab9a5307dd9630ff35d270
pablocerve <pablo_cerve@hotmail.com>
2017-07-30 19:54:43 -0300

    run station 222, 333, SLF2

--- code/data-quality/parser_vwc.py
+++ code/data-quality/parser_vwc.py
@@ -1,4 +1,9 @@
-from parser import *
+from parser import Parser
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.style.use('ggplot')
 
 class ParserVWC(Parser):
 	def __init__(self):
@@ -12,3 +17,11 @@ class ParserVWC(Parser):
 		s_line = line.split()
 		self._parse_columns(s_line)
 		self.parsing_header = False
+
+	def plot(self):
+		ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+		ts = ts.cumsum()
+		df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list('ABCD'))
+		df = df.cumsum()
+		plt.figure(); df.plot();
+		plt.savefig("1.png")
--- code/data-quality/script.py
+++ code/data-quality/script.py
@@ -11,11 +11,14 @@ import sys
 def parse_vwc_files(folder):
 	filenames = ["vwc_1202.dat", "vwc_1203.dat", "vwc_1204.dat", "vwc_1205.dat", "vwc_222.smet", "vwc_333.smet", "vwc_SLF2.smet"]
 	for filename in filenames:
-		_parse_and_process(ParserVWC(), folder, filename)
+		parser = ParserVWC()
+		_parse_and_process(parser, folder, filename)
+		parser.plot()
 		print
 
 def parse_smet_files(key, folder):
-	station_ids = ["1202", "1203", "1204", "1205", "222", "333", "SLF2"]
+	# station_ids = ["1202", "1203", "1204", "1205", "222", "333", "SLF2"]
+	station_ids = ["222", "333", "SLF2"]
 	for station_id in station_ids:
 		filename = key + "_" + station_id + ".smet"
 		_parse_and_process(ParserSMET(), folder, filename)
