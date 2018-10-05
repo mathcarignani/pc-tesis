c8bc314add575d2cf316354fac4d240f18aa5152
pablo-cerve <pablo@theappraisallane.com>
2018-10-05 04:06:46 -0300

    Copy current Slide Filter implementation before changes

---
+++ cpp_project/include/coders/linear/slide_filter(old)/decoder_slide_filter.h
@@ -0,0 +1,38 @@
+
+#ifndef CPP_PROJECT_DECODER_SLIDE_FILTER_H
+#define CPP_PROJECT_DECODER_SLIDE_FILTER_H
+
+#include "decoder_cols.h"
+
+#if MASK_MODE
+
+#include "coder_utils.h"
+#include "SlideFiltersEntry.h"
+#include "slide_filter_window.h"
+
+class DecoderSlideFilter: public DecoderCols {
+
+private:
+    int max_window_size_bit_length;
+    Column* column;
+    SlideFilterWindow* m_pApproxData;
+
+//    int current_window_size;
+//    std::string current_value;
+
+    std::vector<std::string> decodeDataColumn() override;
+    SlideFiltersEntry* decodeEntry();
+    SlideFiltersEntry* getAt(std::vector<SlideFiltersEntry*> & m_pCompressData, int position);
+    std::vector<DataItem> decompress();
+
+//    void decodeWindow(std::vector<std::string> & column);
+//    void createWindow(std::vector<std::string> & column, std::string previous_value);
+
+public:
+    using DecoderCols::DecoderCols;
+    void setCoderParams(int max_window_size_);
+};
+
+#endif // MASK_MODE
+
+#endif //CPP_PROJECT_DECODER_SLIDE_FILTER_H
