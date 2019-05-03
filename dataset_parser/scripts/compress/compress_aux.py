16b51492814ed0cf6316c0538b3bbaaa428d82cc
pablo-cerve <pablo@theappraisallane.com>
2019-05-03 01:55:34 -0300

    Set a single window size value for the CoderSF in the compress_aux.py file.

--- dataset_parser/scripts/compress/compress_aux.py
+++ dataset_parser/scripts/compress/compress_aux.py
@@ -84,7 +84,7 @@ MASK_MODE_CODERS_ARRAY = [
     {
         'name': 'CoderSF',
         'o_folder': 'sf',
-        'params': {'window_size': WINDOW_SIZES}
+        'params': {'window_size': [0]}  # window_size param doesn't matter
     }
 ]
 
