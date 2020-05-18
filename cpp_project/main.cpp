2bd10078279cdca5d357cb6eee4ef6c08076502e
pablo-cerve <pablo@theappraisallane.com>
2020-05-18 01:36:21 -0300

    [cpp_project] Changed main.cpp so that path args contain the full path in a single string

--- cpp_project/main.cpp
+++ cpp_project/main.cpp
@@ -9,13 +9,13 @@
 // USAGE - EXAMPLES:
 //
 // (1) MASK_MODE == 0, Code with CoderBase (no window_size param, no err params)
-// exe_path 0 c input_path input_filename output_path output_filename CoderBase
+// exe_path 0 c input_path output_path CoderBase
 //
 // (2) MASK_MODE == 3, Code with CoderPCA
-// exe_path 3 c input_path input_filename output_path output_filename CoderPCA window_size err1 err2 ... errN
+// exe_path 3 c input_path output_path CoderPCA window_size err1 err2 ... errN
 //
 // (3) MASK_MODE == 0, Decode
-// exe_path 0 d input_path input_filename output_path output_filename
+// exe_path 0 d input_path output_path
 //
 int main(int argc, char *argv[]){
     assert(Constants::validMaskMode());
@@ -38,32 +38,32 @@ int main(int argc, char *argv[]){
     std::string action = argv[2];
     assert(action == "c" or action == "d");
 
-    Path input_path = Path(argv[3], argv[4]);
-    Path output_path = Path(argv[5], argv[6]);
+    Path input_path = Path(argv[3]);
+    Path output_path = Path(argv[4]);
 
     if (action == "d"){
-        assert(argc == 7);
+        assert(argc == 5);
         Scripts::decode(input_path, output_path);
         return 0;
     }
 
     // action == "c"
-    std::string coder_name = argv[7];
+    std::string coder_name = argv[5];
     std::vector<std::string> coders_array{"CoderBase", "CoderPCA", "CoderAPCA", "CoderPWLH",
                                           "CoderPWLHInt", "CoderCA", "CoderSF", "CoderFR",
                                           "CoderGAMPS", "CoderGAMPSLimit"};
 
     if (coder_name == "CoderBase"){
-        assert(argc == 8);
+        assert(argc == 6);
         Scripts::codeBase(input_path, output_path);
         return 0;
     }
 
     assert(StringUtils::stringInList(coder_name, coders_array));
-    assert(argc >= 10);
-    int window_size = atoi(argv[8]);
+    assert(argc >= 8);
+    int window_size = atoi(argv[6]);
     std::vector<int> error_thresholds_vector;
-    for(int i=9; i < argc; i++){ error_thresholds_vector.push_back(atoi(argv[i])); }
+    for(int i=7; i < argc; i++){ error_thresholds_vector.push_back(atoi(argv[i])); }
 
     Scripts::code(coder_name, input_path, output_path, window_size, error_thresholds_vector);
     return 0;
