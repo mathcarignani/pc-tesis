61f1582bef60c0879adddce9cb803a558542447a
pablo-cerve <pablo@theappraisallane.com>
2020-10-27 14:37:21 -0300

    [python] Minor string change in GAMPSExample

--- dataset_parser/scripts/informe/examples/gamps/gamps_example.py
+++ dataset_parser/scripts/informe/examples/gamps/gamps_example.py
@@ -16,9 +16,9 @@ class GAMPSExample(ExamplesBase):
         self.arrows = []
         self.patches = []
 
-        self.label_orig = 'sample (column 1)'
-        self.label_enco = 'sample (column 2)'
-        self.label_deco = 'sample (column 3)'
+        self.label_orig = 'sample (signal 1)'
+        self.label_enco = 'sample (signal 2)'
+        self.label_deco = 'sample (signal 3)'
         self.epsilon = 0
 
         self.gamps1("gamps1.pdf", 0, 11)
@@ -32,9 +32,9 @@ class GAMPSExample(ExamplesBase):
         self.decoded =  [1, 1, 1, 1, 1, 2/3, 2/3, 1, 1, 1, 1, 1]
         self.encoded_points = [(0,1), (1,1), (2,1), (3,1), (4,1), (5,4/3), (6,4/3), (7,1), (8,1), (9,1), (10,1), (11,1)]
 
-        self.label_orig = 'encoded sample (column 1)'
-        self.label_enco = 'encoded sample (column 2)'
-        self.label_deco = 'encoded sample (column 3)'
+        self.label_orig = 'encoded sample (signal 1)'
+        self.label_enco = 'encoded sample (signal 2)'
+        self.label_deco = 'encoded sample (signal 3)'
         self.common(filename, step, index)
 
 
